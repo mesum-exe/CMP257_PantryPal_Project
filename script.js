f20eaf9563c67cbff963229139bf62ca7481d5a9
mesum-exe <mesum.a680@gmail.com>
2025-10-21 21:58:00 +0400

    Added Dashboard and JavaScript File
    Created a basic dashboard structure with placeholder values.
    JavaScript file includes code to fetch and add navbar and footer components from seperate files.

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Used for loading the Navbar onto every page without needing to copy and paste it.
+fetch('navbar.html')
+    .then(response => response.text())  // converts navbar.html into only HTML text
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    });
+
+// Used for loading the Footer onto every page without needing to copy and paste it.
+fetch('footer.html')
+    .then(response => response.text())  // converts footer.html into only HTML text
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    });
